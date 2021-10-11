df1662d0ee9e87d32f474ac526ac42b5834f2717
Kishore S <skishore1843@gmail.com>
2021-10-12 00:52:18 +0530

    VSS A20 First Commit
    Completed till 3. Arguments

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # arrowFunction
 Normal vs Arrow function of JS
+
+## Syntax
+
+### Normal function
+```
+let x =  function function_name(parameters){
+// body of the normal function
+};
+```
+### Arrow function
+```
+let x = (parameters) => {
+	// body of the arrow function
+};
+```
+
---
+++ index.js
@@ -0,0 +1,56 @@
+// 1. Hoisting
+
+// normal function declaration
+
+console.log(hello()); // Hello World
+
+function hello(){
+    return "Hello World";
+}
+
+// normal function expression
+
+// console.log(helloExp()); // ReferenceError: Cannot access 'helloExp' before initialization
+let helloExp = function(){
+    return "Hello World from helloExp"
+}
+console.log(helloExp()); // Hello World from helloExp
+
+// arrow function
+
+// console.log(helloArrow()); //ReferenceError: Cannot access 'helloArrow' before initialization
+let helloArrow = () =>{
+    return "Hello World from helloArrow";
+}
+console.log(helloArrow()); // Hello World from helloArrow
+
+// 2. this Keyword
+
+let user = {
+    uName: "Hinata",
+    name1:()=>{
+        console.log("this name from arrow function: "+this.uName);
+    },
+    name2(){
+        console.log("this name from normal function: "+this.uName);
+    }
+};
+
+user.name1(); // this name from arrow function: undefined (No this keyword for arrow function)
+user.name2(); // this name from normal function: Hinata
+
+// 3. Arguments
+
+let user2 = {
+    show:()=>{
+        console.log("Arguments from arrow function: ",...arguments);
+    },
+    show2(){
+        console.log("Arguments from normal function: ",...arguments);
+    }
+};
+
+// user2.show(1,2,3); // ReferenceError: arguments is not defined (Arrow functions do not have arguments)
+user2.show2(1,2,3); // Arguments from normal function:  1 2 3
+
+// let fun1:(fun, args) => fun(...args);
